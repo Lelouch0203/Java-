c196fd1f15670240cd974c33290d2f1a87dbcbb6
Lelouch0203 <jaiv2k7@gmail.com>
2024-12-11 11:34:20 +0530

    Uploaded

---
+++ CollegeExercises/SpecialNumbers/CircularPrime.java
@@ -0,0 +1,13 @@
+package CollegeExercises.SpecialNumbers;
+
+import java.util.Scanner;
+
+public class CircularPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num = sc.nextInt();
+        
+
+    }
+}
---
+++ CollegeExercises/SpecialNumbers/HappyNumber.java
@@ -0,0 +1,31 @@
+package CollegeExercises.SpecialNumbers;
+
+import java.util.Scanner;
+
+public class HappyNumber {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num = sc.nextInt();
+        int digits=0;
+        int n= num;
+       int  sum=0;
+        
+
+        while (num>9) {
+            while (num>0) {
+                digits= num%10;
+                sum = sum+(digits*digits);
+                num/=10;
+            }
+            num=sum;
+            sum=0;
+            }
+            if(num==1){
+                System.out.println(n+" is a capricorn number");
+            }
+            else{
+                System.out.println(n+" is not a capricorn number");
+            }
+    }
+}
